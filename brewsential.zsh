62187b9645a28a500be80be2264713f41001a876
Sky Moore <i@msky.me>
2025-04-25 18:52:44 +0100

    feat: starting list of essential packages

---
+++ brewsential.zsh
@@ -0,0 +1,45 @@
+#!/bin/zsh
+
+
+brew install \
+    stats \
+    jordanbaird-ice \
+    vim \
+    freelens \
+    maccy \
+    iterm2 \
+    docker \
+    docker-buildx \
+    docker-compose \
+    firefox \
+    gpg-suite-no-mail \
+    chai \
+    curl \
+    pv \
+    openssh \
+    openssl \
+    watch \
+    oversight \
+    vscodium \
+    cursor \
+    ngrok \
+    ca-certificates \
+    python3 \
+    go \
+    dyff \
+    kubernetes-cli \
+    kustomize \
+    kubectx \
+    awscli \
+    argocd \
+    black \
+    httpie \
+    wget \
+    utm \
+    vlc \
+    dbeaver-community \
+    helm \
+    htop \
+    ipcalc \
+
+
